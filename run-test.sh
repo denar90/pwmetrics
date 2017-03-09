91547a5848d9cf8d75e330a9d77e04bca740179c
Artem Denysov <denysov.artem@gmail.com>
2017-03-10 00:57:07 +0200

    add test runner

---
+++ run-test.sh
@@ -0,0 +1,13 @@
+if [[ $RUN_RECIPES = "1" ]];
+  then yarn run gulp-example;
+else
+  npm run build &&
+  if [[ $(node -v) =~ ^v4.* ]]; then
+    node --harmony test
+  else
+    test
+  fi
+fi
+
+test
+	node_modules/.bin/mocha --require test/setup/common.js
