89f8f98c7fe12f4b11a1359cf17dd6006d808ebe
Artem Denysov <denysov.artem@gmail.com>
2017-03-10 23:06:20 +0200

    test

--- run-test.sh
+++ run-test.sh
@@ -2,8 +2,8 @@ if [[ $RUN_RECIPES = "1" ]];
   then yarn run gulp-example;
 else
   npm run build &&
-  if [[ $(node -v) =~ ^v6.* ]]; then
-    ./node_modules/.bin/mocha --harmony-async-await --require ./test/setup/common.js
+  if [[ $(node -v) =~ ^v7.* ]]; then
+    ./node_modules/.bin/mocha --harmony --require ./test/setup/common.js
   else
     ./node_modules/.bin/mocha --require ./test/setup/common.js
   fi
